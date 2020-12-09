cc29cdc0be4ffc54d3d5a576366ecf1d3738ccc8
unknown <lydia291@gmail.com>
2020-12-08 20:50:47 -0500

    after tutor help

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,3 +2,27 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+function buildTable(data){
+    distbody = d3.select("tbody");
+    distbody.html("");
+    data.forEach((objectrow) => {
+        var row = distbody.append("tr");
+        Object.values(objectrow).forEach((rowvalues) => {
+            var cells = row.append("td");
+            cells.text(rowvalues);
+        })
+    })
+}
+
+buildTable(tableData);
+
+function filterData(){ 
+    var tableData = data;
+    var date = d3.select("#datetime").property("value");
+    filteredData = tableData.filter(objectrow => objectrow.datetime === date);
+    buildTable(filteredData);
+
+}
+
+d3.selectAll("#filter-btn").on("click", filterData);
+
--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -2,3 +2,46 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+function buildTable(data){
+    distbody = d3.select("tbody");
+    distbody.html("");
+    data.forEach((objectrow) => {
+        var row = distbody.append("tr");
+        Object.values(objectrow).forEach((rowvalues) => {
+            var cells = row.append("td");
+            cells.text(rowvalues);
+        })
+    })
+}
+
+buildTable(tableData);
+
+var filtered = {};
+function createFilters(){
+    var changedElement = d3.select(this).select("input");
+    var datavalue = changedElement.property("value");
+    var filterID = changedElement.attr("id");
+    if (datavalue){
+        filtered[filterID] = datavalue;
+
+    }
+    else{
+        delete filtered[filterID];
+    }
+    filterData();
+}
+
+function filterData(){ 
+    var tableData = data;
+    Object.entries(filtered).forEach(([key, value]) => {
+        filteredData = tableData.filter(row => row[key] === value);
+    });
+    buildTable(filteredData);
+    // var date = d3.select("#datetime").property("value");
+    // filteredData = tableData.filter(objectrow => objectrow.datetime === date);
+    // buildTable(filteredData);
+
+}
+
+d3.selectAll(".filter").on("change", createFilters);
+
